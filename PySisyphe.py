a10bbdd69c98e4379879b4260fb68c11776dc67c
PySisyphe <151433553+PySisyphe@users.noreply.github.com>
2025-06-20 07:52:59 +0200

    Add files via upload

--- PySisyphe.py
+++ PySisyphe.py
@@ -1,50 +1,230 @@
 """
-    External packages/modules
+External packages/modules
+-------------------------
 
-        Name            Link                                                        Usage
-
-        PyQt5           https://www.riverbankcomputing.com/software/pyqt/           Qt GUI
+    - PyQt5, Qt GUI, https://www.riverbankcomputing.com/software/pyqt/
+    - PyQtDarkTheme, dark theme management for win32 platform, https://pyqtdarktheme.readthedocs.io/en/stable/index.html
+    - pywinstyles, customize window title bar for win32 platform, https://github.com/Akascape/py-window-styles
+    - vtk, visualization, https://vtk.org/
 """
 
+
 import os
+
+# Disable debugger warnings from IPython frozen modules
+os.environ['PYDEVD_DISABLE_FILE_VALIDATION'] = '1'
+# Disable tensorflow warnings
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+from os.path import exists
+from os.path import join
+from os.path import splitext
+
+import sys
+
 import ctypes
+
 from sys import argv, exit, platform
 
+# noinspection PyUnresolvedReferences
+from multiprocessing import freeze_support
+
+import argparse
+
+# fix Qt crash on macOS BigSur platform
 if platform == 'darwin':
     os.environ["QT_MAC_WANTS_LAYER"] = "1"
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtCore import QSettings
+from PyQt5.QtCore import QLocale
+from PyQt5.QtCore import qInstallMessageHandler
+from PyQt5.QtCore import QtMsgType
+from PyQt5.QtCore import QMessageLogContext
+from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QPalette
+from PyQt5.QtGui import QColor
+
+from vtk import vtkObject
+
+from Sisyphe.core.sisypheVolume import SisypheVolume
+from Sisyphe.gui.dialogSplash import DialogSplash
+
 from PyQt5.QtWidgets import QApplication
 
-from Sisyphe.gui.dialogSplash import DialogSplash
-from Sisyphe.gui.windowSisyphe import WindowSisyphe
+if platform == 'win32':
+    # noinspection PyUnresolvedReferences
+    import pywinstyles
+    # noinspection PyUnresolvedReferences
+    import qdarktheme
 
 """
-    PySisyphe
+PySisyphe main
+
+Last revision: 13/03/2025
 """
+
+BACKGROUND: QColor | None = None
+PALETTE: QPalette | None = None
+
+"""
+functions
+~~~~~~~~~
+
+    - getPalette
+    - getBackgroundAsQColor
+    - getBackgroundAsStr
+    - getForegroundAsQColor
+    - getForegroundAsStr
+    - qtMessageHandler
+    
+"""
+
+def getPalette() -> QPalette:
+    return PALETTE
+
+def getBackgroundAsQColor() -> QColor:
+    return PALETTE.base().color()
+
+def getBackgroundAsStr() -> str:
+    cl = PALETTE.base().color()
+    return '#{:02x}{:02x}{:02x}'.format(cl.red(), cl.green(), cl.blue())
+
+def getForegroundAsQColor() -> QColor:
+    return PALETTE.button().color()
+
+def getForegroundAsStr() -> str:
+    cl = PALETTE.button().color()
+    return '#{:02x}{:02x}{:02x}'.format(cl.red(), cl.green(), cl.blue())
+
+def updateWindowTitleBarColor(window):
+    if platform == 'win32':
+        pywinstyles.change_header_color(window, getBackgroundAsStr())
+        QApplication.processEvents()
+
+# noinspection PyUnusedLocal,PyShadowingBuiltins
+def qtMessageHandler(type: QtMsgType, context: QMessageLogContext, msg: str):
+    # disable qt stdout warnings to avoid console output in frozen code
+    pass
+
 
 if __name__ == "__main__":
 
-    app = QApplication(argv)
-    settings = QSettings('PySisyphe', 'PySisyphe')
+    """
+    Disable Qt, vtk and python console stdout
+    """
 
-    if platform == 'darwin':
-        QApplication.setAttribute(Qt.AA_DontShowIconsInMenus, True)
-        QApplication.setAttribute(Qt.AA_DontUseNativeMenuBar, False)
-    elif platform == 'win32':
+    # < Revision 20/02/2025
+    # redirect python stdout and stderr to null file to avoid console output in frozen code
+    if sys.stdout is None: sys.stdout = open(os.devnull, 'w')
+    if sys.stderr is None: sys.stderr = open(os.devnull, 'w')
+    # Revision 20/02/2025 >
+
+    # < Revision 03/03/2025
+    # disable qt stdout warnings to avoid console output in frozen code
+    qInstallMessageHandler(qtMessageHandler)
+    # Revision 03/03/2025 >
+
+    # < Revision 19/03/2025
+    # disable vtk stdout warnings to avoid console output in frozen code
+    # noinspection PyArgumentList
+    vtkObject.GlobalWarningDisplayOff()
+    # Revision 19/03/2025 >
+
+    """
+    Enable support for multiprocessing in frozen code
+    """
+
+    # < Revision 05/11/2024
+    # enable support for multiprocessing in frozen code
+    freeze_support()
+    # Revision 05/11/2024 >
+
+    """
+    Parse input arguments
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--open', help='Open PySisyphe volume (*.xvol)', nargs="*", type=str)
+    args = parser.parse_args()
+
+    """
+    Create application
+    """
+    from Sisyphe.gui.windowSisyphe import WindowSisyphe
+
+    app = QApplication(argv)
+    # < Revision 18/02/2025
+    QApplication.setApplicationName('PySisyphe')
+    QApplication.setWindowIcon(QIcon(join(WindowSisyphe.getDefaultIconDirectory(), 'pysisyphe.png')))
+    # Revision 18/02/2025 >
+
+    # < Revision 07/12/2024
+    QLocale.setDefault(QLocale(QLocale.English, QLocale.UnitedStates))
+    # Revision 07/12/2024 >
+
+    # noinspection PyTypeChecker
+    QApplication.setAttribute(Qt.AA_DontShowIconsInMenus, True)
+    # noinspection PyTypeChecker
+    QApplication.setAttribute(Qt.AA_DontUseNativeMenuBar, False)
+
+    if platform == 'win32':
         # High DPI scaling bugfix
         ctypes.windll.shcore.SetProcessDpiAwareness(2)
-        # QApplication.setAttribute(Qt.AA_DisableHighDpiScaling)
+        # Set theme
+        PALETTE = qdarktheme.load_palette('auto')
+        c = ('background-color: {0}; '
+             'border-color: {0}; '
+             'border: 0px;').format(getBackgroundAsStr())
+        qss = 'QToolBar { ' + c + ' }'
+        c = ('border-color: {0}; '
+             'border: 0px; '
+             'spacing: 20px;').format(getBackgroundAsStr())
+        qss += ' QMenuBar { ' + c + ' }'
+        c = ('border-style: solid; '
+             'border-radius: 10px; '
+             'border-width: 1px; '
+             'border-color: {0};').format(getForegroundAsStr())
+        qss += ' QMenu { ' + c + ' }'
+        qss += ' QToolTip { color: #000000; background-color: #FFFFE0; border: 0px; font-size: 8pt; }'
+        c = 'border-style: none; background-color: {0};'.format(getBackgroundAsStr())
+        qss += ' QStatusBar { ' + c + ' }'
+        # qss += ' QCheckBox, QComboBox, QLabel, QLineEdit, QPushButton, QListWidget, QTreeWidget { margin: 5px; }'
+        qss += ' QCheckBox, QComboBox, QLineEdit, QPushButton, QListWidget, QTreeWidget { margin: 5px; }'
+        qss += ' QGroupBox { margin: 5px; font-size: 8pt; font-weight: normal; }'
+        qss += ' QPushButton#RoundedButton { margin: 0px; }'
+        qss += ' QPushButton#iconPushButton { margin: 0px; }'
+        qss += ' QLabel#iconButton { margin: 0px; }'
+        qss += ' QLabel#colorPushButton { margin: 0px; }'
+        qss += ' QLabel#visibilityButton { margin: 0px; }'
+        qss += ' QLabel#lockButton { margin: 0px; }'
+        qss += ' QLabel#opacityButton { margin: 0px; }'
+        qss += ' QLabel#widthButton { margin: 0px; }'
+        qss += ' QLabel#widthButton { margin: 0px; }'
+        qdarktheme.setup_theme('auto', additional_qss=qss)
+    else:
+        PALETTE = app.palette()
+        # Qt bug fix, lost macOS style when button height > 30px
+        app.setStyleSheet('QPushButton { max-height: 30px; }')
+
+    """
+    Set up main window
+    """
 
     splash = DialogSplash()
+    if platform == 'win32': updateWindowTitleBarColor(splash)
     splash.buttonVisibilityOff()
-    splash.progressBarVisibilityOff()
+    splash.progressBarVisibilityOn()
     splash.show()
-    QApplication.processEvents()
 
-    main = WindowSisyphe()
-    main.show()
+    main = WindowSisyphe(splash)
+    if platform == 'win32': updateWindowTitleBarColor(main)
     splash.close()
+
+    if args.open is not None:
+        for filename in args.open:
+            if exists(filename):
+                if splitext(filename)[1] == SisypheVolume.getFileExt():
+                    main.open(filename)
 
     exit(app.exec_())
--- version.py
+++ version.py
@@ -1,71 +1,76 @@
 """
-    External packages/modules
-
-        Name            Link                                                        Usage
-
-        BeautifulSoup   https://www.crummy.com/software/BeautifulSoup/bs4/doc/      HTML parser
-        Requests        https://requests.readthedocs.io/en/latest/                  Simple HTTP library
+External packages/modules
+-------------------------
 """
 
-from os.path import join
 from os.path import dirname
 from os.path import exists
 from os.path import abspath
 
-from bs4 import BeautifulSoup as bs
+from binascii import unhexlify
 
-import requests as rq
+from Sisyphe.lib.mega.mega import Mega
 
 from xml.dom import minidom
 
-__version__ = "0.1.0"
+__version__ = "0.9.1"
 
-_VERSIONURL = 'https://www.mediafire.com/file/ytynbn92vnohz2c/versions.xml/file'
+_V = b'70797369737970686540676d61696c2e636f6d'
 
 """
-    Functions
-    
-       getMajorVersion() -> int
-       getMinorVersion() -> int
-       getPatchVersion() -> int
-       getVersion() -> str
-       getVersionAsList() -> ist[int, int, int]
-       getVersionAsDict() -> dict[str: int]
-       isOlderThan(version: str | list[int, int, int] | dict[str: int]) -> bool
-       isNewerThan(version: str | list[int, int, int] | dict[str: int]) -> bool
-       isCurrentVersion(version: str | list[int, int, int] | dict[str: int]) -> bool
-       getVersionFromHost(timeout: float = 5.0) -> str
-       isUpToDateVersion() -> bool
-       getVersionHistoryFromHost() -> dict[str: str]
-       getUrlsToUpdate(history: dict[str: str] | None = None) -> list[tuple[str: str]]
+Functions
+~~~~~~~~~
+
+   - getMajorVersion
+   - getMinorVersion
+   - getPatchVersion
+   - getVersion
+   - getVersionAsList
+   - getVersionAsDict
+   - isOlderThan
+   - isNewerThan
+   - isCurrentVersion
+   - getVersionFromHost
+   - isUpToDateVersion
+   - getVersionHistoryFromHost
+   - getUrlsToUpdate
        
-    Creation: 04/11/2023
+Creation: 04/11/2023
+Last revision: 30/10/2024
 """
+
 
 def getMajorVersion() -> int:
     return int(__version__.split('.')[0])
 
+
 def getMinorVersion() -> int:
     return int(__version__.split('.')[1])
+
 
 def getPatchVersion() -> int:
     return int(__version__.split('.')[2])
 
+
 def getVersion() -> str:
     return __version__
 
-def getVersionAsList() -> list[int, int, int]:
+
+def getVersionAsList() -> list[int]:
     return [int(v) for v in __version__.split('.')]
+
 
 def getVersionAsDict() -> dict[str: int]:
     r = dict()
     v = __version__.split('.')
+    # noinspection PyUnresolvedReferences
     r['major'] = int(v[0])
     r['minor'] = int(v[1])
     r['patch'] = int(v[2])
     return r
 
-def isOlderThan(version: str | list[int, int, int] | dict[str: int]) -> bool:
+
+def isOlderThan(version: str | list[int] | dict[str, int]) -> bool:
     if isinstance(version, str): version = [int(v) for v in version.split('.')]
     elif isinstance(version, dict): version = [version['major'], version['minor'], version['patch']]
     if isinstance(version, list):
@@ -78,7 +83,8 @@ def isOlderThan(version: str | list[int, int, int] | dict[str: int]) -> bool:
         return False
     else: raise TypeError('Invalid parameter type {}.'.format(type(version)))
 
-def isNewerThan(version: str | list[int, int, int] | dict[str: int]) -> bool:
+
+def isNewerThan(version: str | list[int] | dict[str: int]) -> bool:
     if isinstance(version, str): version = [int(v) for v in version.split('.')]
     elif isinstance(version, dict): version = [version['major'], version['minor'], version['patch']]
     if isinstance(version, list):
@@ -91,34 +97,27 @@ def isNewerThan(version: str | list[int, int, int] | dict[str: int]) -> bool:
         return False
     else: raise TypeError('Invalid parameter type {}.'.format(type(version)))
 
-def isCurrentVersion(version: str | list[int, int, int] | dict[str: int]) -> bool:
+
+def isCurrentVersion(version: str | list[int] | dict[str, int]) -> bool:
     if isinstance(version, list): version = '.'.join([str(v) for v in version])
     elif isinstance(version, dict): version = '.'.join([str(version[v]) for v in ('major', 'minor', 'patch')])
     if isinstance(version, str): return version == __version__
     else: raise TypeError('Invalid parameter type {}.'.format(type(version)))
 
-def getVersionFromHost(timeout: float = 5.0) -> str:
-    """
-        timeout     float, timeout in seconds, default = 5s
-    """
-    import Sisyphe
-    filename = join(dirname(abspath(Sisyphe.__file__)), 'versions.xml')
-    url = ''
-    # Get html download page from host, search file link
-    r = rq.get(_VERSIONURL, timeout=timeout)
-    if r.status_code == rq.codes.ok:
-        html = r.content
-        soup = bs(html, 'html.parser')
-        item = soup.find_all('a', class_='input popsok')
-        if len(item) > 0:
-            item = item[0]
-            url = item.attrs['href']
-    if url == '': raise IOError('PySisyphe host connection failed.')
-    # Download file
-    r = rq.get(url, timeout=timeout)
-    if r.status_code == rq.codes.ok:
-        with open(filename, 'wb') as file:
-            file.write(r.content)
+
+def getVersionFromHost() -> str:
+    # Connect to host as anonymous & Download host.xml
+    mega = Mega()
+    import Sisyphe.settings
+    try:
+        v1 = unhexlify(_V).decode()
+        v2 = unhexlify(_V[:18]).decode()
+        path = dirname(abspath(Sisyphe.settings.__file__))
+        mega.login(v1, v2)
+        h = mega.find('host.xml')
+        filename = str(mega.download(h, dest_path=path))
+    except: filename = ''
+    version = ''
     if exists(filename):
         doc = minidom.parse(filename)
         root = doc.documentElement
@@ -126,32 +125,10 @@ def getVersionFromHost(timeout: float = 5.0) -> str:
             section = doc.getElementsByTagName('version')
             if len(section) > 0:
                 section = section[0]
-                return section.getAttribute('current')
+                version = section.getAttribute('current')
+    return version
+
 
 def isUpToDateVersion() -> bool:
     version = getVersionFromHost()
     return isCurrentVersion(version)
-
-def getVersionHistoryFromHost() -> dict[str: str]:
-    import Sisyphe
-    filename = join(dirname(abspath(Sisyphe.__file__)), 'versions.xml')
-    if not exists(filename): getVersionFromHost()
-    else:
-        doc = minidom.parse(filename)
-        root = doc.documentElement
-        if root.nodeName == 'host' and root.getAttribute('version') == '1.0':
-            sections = doc.getElementsByTagName('folder')
-            r = dict()
-            if len(sections) > 0:
-                for section in sections:
-                    if section.hasAttribute('version') and section.hasAttribute('url'):
-                        r[section.getAttribute('version')] = section.getAttribute('url')
-            return r
-
-def getUrlsToUpdate(history: dict[str: str] | None = None) -> list[tuple[str: str]]:
-    r = list()
-    if history is None: history = getVersionHistoryFromHost()
-    for version, url in history.items():
-        if isNewerThan(version): r.append((version, url))
-    return r
-
