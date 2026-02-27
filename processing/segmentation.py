b0c95a2f2288caa3457e99745f3e29cf5ba9e2bb
PySisyphe <151433553+PySisyphe@users.noreply.github.com>
2026-02-27 16:05:25 +0100

    Add files via upload

--- processing/segmentation.py
+++ processing/segmentation.py
@@ -7,8 +7,18 @@ External packages/modules
     - SimpleITK, https://simpleitk.org/, Medical image processing
 """
 
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from os.path import join
+from os.path import dirname
+from os.path import abspath
+
+from numpy import clip
 from numpy import min
 from numpy import max
+from numpy import array
+from numpy import repeat
 from numpy import zeros
 from numpy import argwhere
 
@@ -22,15 +32,27 @@ from SimpleITK import ConnectedComponentImageFilter
 
 from sklearn.neighbors import BallTree
 
+from skimage import transform
+
+import torch
+
 from Sisyphe.core.sisypheVolume import SisypheVolume
 from Sisyphe.core.sisypheVolume import SisypheVolumeCollection
 from Sisyphe.core.sisypheVolume import multiComponentSisypheVolumeFromList
 from Sisyphe.core.sisypheImageAttributes import SisypheAcquisition
+from Sisyphe.lib.sam import sam_model_registry
 from Sisyphe.gui.dialogWait import  DialogWait
+
+# to avoid ImportError due to circular imports
+if TYPE_CHECKING:
+    from numpy import ndarray
+    from torch import Tensor
+    from Sisyphe.core.sisypheROI import  SisypheROI
 
 __all__ = ['brainMaskFromProbabilityTissueMaps',
            'probabilityTissueMapsToLabelMap',
-           'nearestNeighborTransformLabelCorrection']
+           'nearestNeighborTransformLabelCorrection',
+           'SegmentAnything']
 
 """
 Functions
@@ -44,8 +66,13 @@ Functions
                                               margin: int = 2,
                                               wait: DialogWait | None = None) -> SisypheVolume
 
+Class
+-----
+
+    - SegmentAnything
+
 Creation: 29/07/2024
-Last revision:
+Last revision: 26/02/2026
 """
 
 # noinspection PyTypeChecker
@@ -57,7 +84,7 @@ def brainMaskFromProbabilityTissueMaps(vols: list[SisypheVolume] | SisypheVolume
     Parameters
     ----------
     vols : list[SisypheVolume] | SisypheVolumeCollection
-        tissue maps, should contain grey and white matter maps
+        tissue maps, should contain gray and white matter maps
     radius : int
         kernel radius
 
@@ -141,7 +168,7 @@ def nearestNeighborTransformLabelCorrection(tmplLabels: SisypheVolume,
                                             wait: DialogWait | None = None) -> SisypheVolume:
     """
     Correct a structure label resulting from a registration-based segmentation with a nearest neighbor transform algorithm.
-    Structure label is corrected with a tissue label map (grey matter, white matter and cerebro-spinal fluid).
+    Structure label is corrected with a tissue label map (gray matter, white matter and cerebro-spinal fluid).
 
     Adapted from: Sdika M., Combining atlas based segmentation and intensity classification with nearest neighbor transform
     and accuracy weighted vote,  Med Image Anal. 2010 Apr;14(2):219-26
@@ -150,9 +177,9 @@ def nearestNeighborTransformLabelCorrection(tmplLabels: SisypheVolume,
     Parameters
     ----------
     tmplLabels : SisypheVolume
-        template tissue label map (grey matter, white matter and cerebro-spinal fluid) registered to subject
+        template tissue label map (gray matter, white matter and cerebro-spinal fluid) registered to subject
     subjLabels : SisypheVolume
-        subject tissue label map (grey matter, white matter and cerebro-spinal fluid)
+        subject tissue label map (gray matter, white matter and cerebro-spinal fluid)
     vstruct : SisypheVolume
         subject structure label to be corrected
     margin : int
@@ -231,3 +258,163 @@ def nearestNeighborTransformLabelCorrection(tmplLabels: SisypheVolume,
                          direction=vstruct.getDirections(),
                          defaultshape=False)
     return r
+
+
+class SegmentAnything(object):
+    """
+    SegmentAnything class
+
+    Description
+    ~~~~~~~~~~~
+
+    Perform segmentation using a Segment Anything Pre-trained Model (MedSAM).
+
+    Reference
+    https://github.com/bowang-lab/MedSAM
+
+    Inheritance
+    ~~~~~~~~~~~
+
+    object -> SegmentAnything
+
+    Creation: 24/02/2026
+    Last revision: 26/02/2026
+    """
+    __slots__ = ['_model', '_image_embed', '_sizex', '_sizey', '_slc', '_orient', '_roi']
+
+    # Special method
+
+    def __init__(self) -> None:
+        """
+        SegmentAnything instance constructor.
+        """
+        import Sisyphe.lib.sam
+        path = abspath(join(dirname(Sisyphe.lib.sam.__file__), 'model', 'medsam_vit_b.pth'))
+        self._model = sam_model_registry['vit_b'](checkpoint=path)
+        self._model.to('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self._model.eval()
+        self._image_embed: Tensor | None = None
+        self._sizex: int = 0
+        self._sizey: int = 0
+        self._slc: int = 0
+        self._orient: int = 0
+        self._roi: SisypheROI | None = None
+
+    """
+    Private attributes
+            
+    _image          ndarray, slice image
+    _sizex          int, slice width
+    _sizey          int, slice height
+    _slc            int, slice index
+    _orient         int, slice orientation 0: axial, 1: coronal, 2: sagittal
+    _roi            SisypheROI
+    """
+
+    # Private method
+
+    @torch.no_grad()
+    def _preprocess(self, img):
+        if len(img.shape) == 2:
+            img = repeat(img[:, :, None], 3, axis=-1)
+        # img.shape (sizey, sizex, 3)
+        img_resize = transform.resize(img, (1024, 1024), order=3, preserve_range=True, anti_aliasing=True).astype('uint8')
+        img_resize = (img_resize - img_resize.min()) / clip(img_resize.max() - img_resize.min(), a_min=1e-8, a_max=None)
+        # img_resize.shape (1024, 1024, 3)
+        img_tensor = torch.tensor(img_resize).float().permute(2, 0, 1).unsqueeze(0).to(self._model.device)
+        # img_tensor.shape (1, 3, 1024, 1024)
+        with torch.no_grad():
+            self._image_embed = self._model.image_encoder(img_tensor)
+        # self._image_embed.shape (1, 256, 64, 64)
+
+    # Public method
+
+    def setROI(self, roi: SisypheROI) -> None:
+        """
+        Set SisypheROI attribute in which to copy the segmentation result.
+
+        Parameters
+        ----------
+        roi : SisypheROI
+            ROI in which to copy the segmentation result
+        """
+        self._roi = roi
+
+    def getROI(self) -> SisypheROI:
+        """
+        Get SisypheROI attribute in which to copy the segmentation result.
+
+        Results
+        -------
+        SisypheROI
+            ROI in which to copy the segmentation result
+        """
+        return self._roi
+
+    def setSlice(self, vol: SisypheVolume, slc: int, orient: int) -> None:
+        """
+        Set slice of the SisypheVolume to segment.
+
+        Parameters
+        ----------
+        vol : SisypheVolume
+            volume to segment
+        slc : int
+            slice index
+        orient: int
+            slice orientation, 0: axial, 1: coronal, 2: sagittal
+        """
+        if vol.hasSameSize(self._roi):
+            if orient == 0: img = vol.getNumpy()[slc, :, :]  # axial
+            elif orient == 1: img = vol.getNumpy()[:, slc, :]  # coronal
+            else: img = vol.getNumpy()[:, :, slc]  # sagittal
+            self._slc = slc
+            self._orient = orient
+            self._sizey, self._sizex = img.shape
+            self._preprocess(img)
+        else: raise ValueError('Size mismatch between volume parameter and ROI.')
+
+    @torch.no_grad()
+    def segment(self, bbox: list[int] | ndarray) -> None:
+        """
+        Use the Segment Anything Pre-trained Model (MedSAM) to perform segmentation of the SisypheVolume attribute in a bounding box.
+        The result is drawn in the SisypheROI attribute.
+
+        Parameters
+        ----------
+        bbox: list[int] | ndarray
+            bounding box (point1 x, point1 y, point2 x, point2 z)
+        """
+        if self._image_embed is not None:
+            if isinstance(bbox, list): bbox = array([bbox])
+            if len(bbox.shape) == 1: bbox = bbox.reshape((1, bbox.shape[0]))
+            bbox1024 = bbox / array([self._sizex, self._sizey, self._sizex, self._sizey]) * 1024
+            bbox_torch = torch.as_tensor(bbox1024,
+                                         dtype=torch.float,
+                                         device=self._image_embed.device)
+            if len(bbox_torch.shape) == 2: bbox_torch = bbox_torch[:, None, :]
+            print(bbox_torch.shape)
+            sparse_embed, dense_embed = self._model.prompt_encoder(points=None,
+                                                                   boxes=bbox_torch,
+                                                                   masks=None)
+            low_res_logits, _ = self._model.mask_decoder(image_embeddings=self._image_embed,
+                                                         image_pe=self._model.prompt_encoder.get_dense_pe(),
+                                                         sparse_prompt_embeddings=sparse_embed,
+                                                         dense_prompt_embeddings=dense_embed,
+                                                         multimask_output=False)
+            low_res_pred = torch.sigmoid(low_res_logits)
+            low_res_pred = torch.nn.functional.interpolate(low_res_pred,
+                                                           size=(self._sizey, self._sizex),
+                                                           mode='bilinear',
+                                                           align_corners=False)
+            low_res_pred = low_res_pred.squeeze().cpu().numpy()
+            if self._orient == 0:  # axial
+                mask = (self._roi.getNumpy()[self._slc, :, :] + (low_res_pred > 0.5)) > 0
+                self._roi.getNumpy()[self._slc, :, :] = mask.astype('uint8')
+            elif self._orient == 1:  # coronal
+                mask = (self._roi.getNumpy()[:, self._slc, :] + (low_res_pred > 0.5)) > 0
+                self._roi.getNumpy()[:, self._slc, :] = mask.astype('uint8')
+            else:  # sagittal
+                mask = (self._roi.getNumpy()[:, :, self._slc] + (low_res_pred > 0.5)) > 0
+                self._roi.getNumpy()[:, :, self._slc] = mask.astype('uint8')
+        else: raise AttributeError('No slice')
